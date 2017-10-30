99d9667b1a34bbd2645b7c26244450245bba8a9a
philip bankier <philipbankier@gmail.com>
2017-10-30 02:25:39 -0400

    changed my personal website to be a sample site

--- app.js
+++ app.js
@@ -120,19 +120,13 @@ app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }))
 
 /**  
  * Primary routes
- */
+ */ 
 app.get('/', pagesController.home);
-app.get('/projects', pagesController.projects);
-app.get('/resume', pagesController.resume);
-app.get('/resources', pagesController.resources);
 app.get('/about', pagesController.about);
-app.get('/rules', pagesController.rules);
 
 /**  
  * Cool Stuff Routes
  */
-app.get('/buddy', pagesController.buddy);
-app.get('/boo', pagesController.boo);
 app.get('/articles', pagesController.articles);
 
 /**   
--- controllers/pages.js
+++ controllers/pages.js
@@ -1,4 +1,3 @@
-
 /**
  * GET /
  * Home page.
@@ -6,56 +5,6 @@
 exports.home = (req, res) => {
   res.render('home', {
     title: 'Home'
-  });
-};
-
-/**
- * GET /
- * Projects page.
- */
-exports.projects = (req, res) => {
-  res.render('projects', {
-    title: 'Projects'
-  });
-};
-
-/**
- * GET /
- * Resources page.
- */
-exports.resources = (req, res) => {
-  res.render('resources', {
-    title: 'Resources'
-  });
-};
-
-/**
- * GET /
- * Resume page.
- */
-exports.resume = (req, res) => {
-  res.render('resume', {
-    title: 'Resume'
-  });
-};
-
-/**
- * GET /
- * Buddy page.
- */
-exports.buddy = (req, res) => {
-  res.render('coolStuff/buddy', {
-    title: 'Buddy'
-  });
-};
-
-/**
- * GET /
- * Boo page.
- */
-exports.boo = (req, res) => {
-  res.render('coolStuff/boo', {
-    title: 'Boo'
   });
 };
 
@@ -76,16 +25,6 @@ exports.articles = (req, res) => {
 exports.about = (req, res) => {
   res.render('about', {
     title: 'About'
-  });
-};
-
-/**
- * GET /
- * Game Rules page.
- */
-exports.rules = (req, res) => {
-  res.render('rules', {
-    title: 'Rules'
   });
 };
 
